eb551c2f42f83257f4fb2a68b731bb53882b06bd
tristan <mercadeo@blackdogpanama.com>
2026-01-05 15:40:21 +0000

    Bump version to 2.0.3

--- src/app/version.ts
+++ src/app/version.ts
@@ -2,6 +2,6 @@
  * Versión de la aplicación
  * Este archivo se genera automáticamente desde package.json
  * Ejecuta: npm run update-version
- * Última actualización: 2026-01-05T15:30:44.482Z
+ * Última actualización: 2026-01-05T15:40:21.047Z
  */
-export const APP_VERSION = '2.0.2';
+export const APP_VERSION = '2.0.3';
