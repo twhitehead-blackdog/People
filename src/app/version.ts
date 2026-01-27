df119c70f95bc335c129729b83ae82777c560714
tristan <mercadeo@blackdogpanama.com>
2026-01-27 14:49:09 +0000

    Update version to 2.0.4 and dependencies

--- src/app/version.ts
+++ src/app/version.ts
@@ -2,6 +2,6 @@
  * Versión de la aplicación
  * Este archivo se genera automáticamente desde package.json
  * Ejecuta: npm run update-version
- * Última actualización: 2026-01-23T07:47:19.827Z
+ * Última actualización: 2026-01-27T14:42:19.513Z
  */
 export const APP_VERSION = '2.0.4';
